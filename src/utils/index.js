42b72ef68116416a0b98a6c5d64c69e1250b65f8
wuhanchu <whcwuhanchu@gmail.com>
2019-10-29 11:59:50 +0800

    delete unused request

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,6 @@ import * as component from "./component"
 import * as componentDict from "./componentDict"
 import * as file from "./file"
 import * as model from "./model"
-import * as request from "./request"
 import * as url from "./url"
 import * as utils from "./utils"
 import * as xlsx from "./xlsx"
@@ -14,7 +13,6 @@ export default {
     componentDict,
     file,
     model,
-    request,
     url,
     utils,
     xlsx
