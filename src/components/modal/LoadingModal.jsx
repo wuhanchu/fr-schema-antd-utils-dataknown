4ae164c6e249bf665add419b8c470efae4548173
wuhanchu <whcwuhanchu@gmail.com>
2019-12-20 17:56:14 +0800

    add clone function

---
+++ src/components/modal/LoadingModal.jsx
@@ -0,0 +1,26 @@
+import React, {Fragment} from "react"
+import {Spin, Modal, Spoin, Icon, Row, Col, Typography} from "antd"
+
+const LoadingModal = function (props) {
+    const {width = 120, tip} = props
+    return (
+        <Modal
+            visible={true}
+            centered={true}
+            closable={false}
+            mask={false}
+            footer={null}
+            width={width}
+        >
+            <Spin
+                tip={tip}
+            >
+                <div style={{
+                    width: "100%", height: "100%", margin: 12
+                }}></div>
+            </Spin>
+        </Modal>
+    )
+}
+
+export default React.memo(LoadingModal)
