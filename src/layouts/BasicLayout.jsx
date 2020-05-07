d534476bae00d69f4807dff2206f733f426228ba
wuhanchu <whcwuhanchu@gmail.com>
2020-05-07 18:31:05 +0800

    commit

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -55,11 +55,11 @@ const BasicLayout = props => {
      */
 
     useEffect(() => {
-        if (dispatch) {
-            dispatch({
-                type: 'user/fetchCurrent',
-            });
-        }
+        // if (dispatch) {
+        //     dispatch({
+        //         type: 'user/fetchCurrent',
+        //     });
+        // }
     }, []);
     /**
      * init variables
